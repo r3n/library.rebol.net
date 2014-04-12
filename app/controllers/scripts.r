3c3808f4f685b7ba51333334889b878c7a758554
Christopher Ross-Gill <chris.rg@ross-gill.com>
2014-04-12 00:43:40 -0400

    CONTENT: First pass at supporting existing documents.

--- app/controllers/scripts.r
+++ app/controllers/scripts.r
@@ -11,17 +11,46 @@ route () to %scripts [
 ]
 
 route (script: string! [wordify]) to %script [
-	verify [
-		description: attempt [
-			require %shell/curl.r
-			curl/fail join http://www.rebol.org/download-a-script.r?script-name= [script %.r]
+	get [
+		if verify [
+			description: attempt [
+				require %shell/curl.r
+				curl/fail join http://www.rebol.org/download-a-script.r?script-name= [script %.r]
+			][
+				reject 404 "Unable to Locate Script"
+			]
 		][
-			reject 404 "Unable to Locate Script"
+			where %.r [
+				print description
+			]
 		]
 	]
 
-	get []
+	get %,docs [
+		if verify [
+			document: attempt [
+				require %shell/curl.r
+				curl/fail join http://www.rebol.org/doc-download.r?format=plain&script-name= [script %.r]
+			][
+				reject 404 "Unable to Locate Document"
+			]
 
-	get %.r [print description]
+			parse document [thru "Downloaded On" thru "^/^/" document: to end][
+				either parse document "<!DOCTYPE" [
+					reject 404 "Document Not Available"
+				][
+					reject 400 "Could Not Parse Document"
+				]
+			]
+		][
+			where/else %.rmd [
+				print head document
+			][
+				require %makedoc/makedoc.r
+				document: load-doc document
+				render %doc
+			]
+		]
+	]
 ]
 
